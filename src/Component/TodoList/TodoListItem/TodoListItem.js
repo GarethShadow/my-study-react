736977d9c918b7bbc8f248bff9b69b5108ce26be
Alexander <mishinall1991@gmail.com>
2022-02-09 20:38:08 +0200

    CLASS_COMPONENTS

--- src/Component/TodoList/TodoListItem/TodoListItem.js
+++ src/Component/TodoList/TodoListItem/TodoListItem.js
@@ -1,17 +1,22 @@
+import React, { Component } from "react";
 import "./StyleTodoListItem.css";
 import TodoListItemButton from "../TodoListItemButton";
 
-const TodoListItem = (props) => {
-    const {label, important = false} = props;
-    const style = {
-        color: important ? "tomato" : "black"
-    };
-    return (
-        <div className="todo-list-item__block">
-            <span className="todo-list__item-style" style={style}>{label}</span>
-            <TodoListItemButton />
-        </div>
-    );
-};
+class TodoListItem extends Component {
+    render() {
+        const {label, important = false} = this.props;
+
+        const style = {
+            color: important ? "tomato" : "black"
+        };
+
+        return (
+            <div className="todo-list-item__block">
+                <span className="todo-list__item-style" style={style}>{label}</span>
+                <TodoListItemButton/>
+            </div>
+        );
+    }
+}
 
 export default TodoListItem;
