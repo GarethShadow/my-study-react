282207d2c710b3b00bc2056c40d1dead62b22026
Alexander <58294180+GarethShadow@users.noreply.github.com>
2022-02-09 20:39:07 +0200

    CLASS_COMPONENTS (#6)

--- src/Component/StatusFilterItem/StatusFilterItem.js
+++ src/Component/StatusFilterItem/StatusFilterItem.js
@@ -1,13 +1,16 @@
+import React, {Component} from "react";
 import "./StyleStatusFilterItem.css";
 
-const StatusFilterItem = () => {
-    return (
-        <div className="status-item__container">
-            <button className="filter-btn">All</button>
-            <button className="filter-btn">Active</button>
-            <button className="filter-btn">Done</button>
-        </div>
-    );
-};
+class StatusFilterItem extends Component {
+    render() {
+        return (
+            <div className="status-item__container">
+                <button className="filter-btn">All</button>
+                <button className="filter-btn">Active</button>
+                <button className="filter-btn">Done</button>
+            </div>
+        );
+    }
+}
 
 export default StatusFilterItem;
