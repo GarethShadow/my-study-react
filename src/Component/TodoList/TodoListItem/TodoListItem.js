877c8b02044a2d8343bfe0d8b56c7fa162cc94ff
Alexander <58294180+GarethShadow@users.noreply.github.com>
2022-02-09 21:03:00 +0200

    EVENT_HANDLING (#7)

--- src/Component/TodoList/TodoListItem/TodoListItem.js
+++ src/Component/TodoList/TodoListItem/TodoListItem.js
@@ -1,8 +1,13 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import "./StyleTodoListItem.css";
 import TodoListItemButton from "../TodoListItemButton";
 
 class TodoListItem extends Component {
+
+    onLabelClick = () => {
+        console.log(`Done: ${this.props.label}`);
+    };
+
     render() {
         const {label, important = false} = this.props;
 
@@ -12,7 +17,7 @@ class TodoListItem extends Component {
 
         return (
             <div className="todo-list-item__block">
-                <span className="todo-list__item-style" style={style}>{label}</span>
+                <span className="todo-list__item-style" style={style} onClick={this.onLabelClick}>{label}</span>
                 <TodoListItemButton/>
             </div>
         );
