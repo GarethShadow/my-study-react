2f2e1fd396cf5c996374dde9ddd634ea5b0ff8e8
Alexander <mishinall1991@gmail.com>
2022-02-09 21:02:21 +0200

    EVENT_HANDLING

--- src/Component/Header/HeaderDate/HeaderDate.js
+++ src/Component/Header/HeaderDate/HeaderDate.js
@@ -1,7 +1,7 @@
 import "./StyleHeaderDate.css";
 
 const HeaderDate = () => {
-    return <p classname="header-date__text">1 more to do, 3 done</p>;
+    return <p className="header-date__text">1 more to do, 3 done</p>;
 };
 
 export default HeaderDate;
